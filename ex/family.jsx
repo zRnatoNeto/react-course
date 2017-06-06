e1291a672ca129b4335508f9c6730b01e822a203
Renato Neto <rnato.netoo@gmail.com>
2017-06-05 23:49:34 -0300

    class 5

---
+++ ex/family.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Member from './member';
+
+export default props => (
+    <div>
+        <Member name='Renato' lastName='Abreu' />
+        <Member name='Larissa' lastName='Abreu' />
+        <Member name='Reinaldo' lastName='Abreu' />
+    </div>
+);
