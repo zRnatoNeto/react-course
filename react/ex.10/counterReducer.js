030ee6852b538678afa538e85979d7811ad352e8
Renato Neto <rnato.netoo@gmail.com>
2018-02-04 21:16:25 -0200

    redux ex 2

---
+++ react/ex.10/counterReducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = { step: 1, number: 0 };
+
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'INC':
+      return { ...state, number: state.number + state.step };
+      break;
+    case 'DEC':
+      return { ...state, number: state.number - state.step };
+      break;
+    case 'STEP_CHANGED':
+      return { ...state, step: Number(action.payload )};
+    default:
+      return state;
+      break;
+  }
+}
